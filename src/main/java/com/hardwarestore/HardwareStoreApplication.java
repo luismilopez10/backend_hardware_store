ed0aac8609d68d45d41b41f1b4659cf8ae463901
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-06-08 21:35:48 -0500

    Swagger annotation added

--- src/main/java/com/hardwarestore/HardwareStoreApplication.java
+++ src/main/java/com/hardwarestore/HardwareStoreApplication.java
@@ -1,10 +1,20 @@
 package com.hardwarestore;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Hardware Store API", version = "1.0.0", description = "Documentation APIs v1.0.0"))
 public class HardwareStoreApplication {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HardwareStoreApplication.class, args);
